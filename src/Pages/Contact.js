9867559ad1c4f61464d6d3fec19c76bd0338b926
Princessayomide53 <94536831+Princessayomide53@users.noreply.github.com>
2022-11-03 23:14:04 -0400

    button:  button tag added and styled

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,14 @@
 import { input } from "postcss";
-import React from "react";
+import React, { useState } from "react";
 
 
 function Contact() {
+    const [setText] = useState('Message sent')
+
+    const popUp = (event) => {
+        setText('Message delivered')
+    }
+
     return (
         <>
         <div className='pl-5'>
@@ -10,33 +16,39 @@ function Contact() {
             <div className='text-[#475467] text-xl leading-7 mb-5 tracking-wide'> <p>Hi there, contact me to ask me anything you have in mind.</p> </div>
             <div className=''>
         <form>
-        <div className='flex justify-between mb-2'>
+        <div className='sm:flex-col lg:justify-between mb-2 md:flex-col lg:flex'>
         <label className='font-medium text-lg leading-5  text-[#344054]'>Firstname </label> 
          <label className='font-medium text-lg leading-5 text-[#344054]'>Lastname </label><br />
          </div>
 
-         <div className='flex space-x-52 mb-5'>
-         <input type="text" placeholder="Enter your first name" className='py-2 outline-slate-500 pl-5 pr-52 bg-[#D0D5DD] rounded-md'/>
-        <input type="text" placeholder="Enter your last name" className='py-2 pl-5 pr-52 outline-slate-500 bg-[#D0D5DD] rounded-md'/>
+         <div className='flex space-x-48 mb-5'>
+         <input type="text" placeholder="Enter your first name" className='py-2 pl-5 pr-56 bg-[#D0D5DD] focus:outline-[#84CAFF] rounded-md'/>
+        <input type="text" placeholder="Enter your last name" className='py-2 pl-5 pr-56 bg-[#D0D5DD] focus:outline-[#84CAFF] rounded-md'/>
         </div> 
         <div className="space-y-2">
             <label className='font-medium text-lg leading-5 text-[#344054]'>Email</label> <br />
-            <input type="password" placeholder="yourname@email.com" className='pt-2 py-2 pl-5 w-10/12 rounded-md bg-[#D0D5DD] outline-slate-500' />
+            <input type="email" placeholder="yourname@email.com" className='pt-2 py-2 pl-5 w-10/12 rounded-md bg-[#D0D5DD] focus:outline-[#84CAFF]' />
         </div>
 
         <div className="space-y-2 mt-5">
             <label className="font-medium text-lg leading-5 mt-5 text-[#344054]">Message</label>
-            <textarea placeholder="Send me a message i will reply as soon as possible" className="pt-2 py-36 pl-5 w-10/12 block rounded-md bg-[#D0D5DD] outline-slate-500"></textarea>
+            <textarea placeholder="Send me a message i will reply as soon as possible" className="pt-2 py-36 pl-5 w-10/12 block rounded-md bg-[#D0D5DD] focus:outline-[#84CAFF]"></textarea>
         </div>
 
         <div className="space-x-5 mt-6">
-            <input type="checkbox" className='w-4 h-4 rounded-md bg-[#D0D5DD] outline-slate-500' />
+            <input type="checkbox" className='w-4 h-4 rounded-lg  ring-2 focus:shadow-current-[#84CAFF] checked:bg-[#fff] focus:bg-[#D1E9FF] outline-[#D1E9FF]' />
             <label className='text-[#475467] text-md tracking-wide leading-6 font-normal'>You agree to providing your data to Princess who may contact you</label>
         </div>
 
-            </form>
-            </div>
+            </form> 
 
+
+
+</div >
+        </div>
+
+        <div className="ml-5 mt-5 ">
+        <button onClick={popUp}  className="w-10/12 h-16 text-md font-semibold leading-6 focus:bg-[#1570EF] hover:bg-[#175CD3] tracking-wide rounded-md disabled:bg-[#B2DDFF] bg-[#1570EF] text-white ">Send message</button>
         </div>
     
         </>
