cf6c4bb9e913b279198b371d475a94c7848740ee
Princessayomide53 <94536831+Princessayomide53@users.noreply.github.com>
2022-11-03 21:09:46 -0400

    padding: padding correctly added to the parent div tag

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -5,8 +5,8 @@ import React from "react";
 function Contact() {
     return (
         <>
-        <div className='p-5'>
-            <h1 className='font-semibold text-4xl text-[#101828] leading-8 mb-5 p-0'>Contact Me</h1>
+        <div className='pl-5'>
+            <h1 className='font-semibold text-4xl text-[#101828] leading-8 mb-5 pt-2'>Contact Me</h1>
             <div className='text-[#475467] text-xl leading-7 mb-5 tracking-wide'> <p>Hi there, contact me to ask me anything you have in mind.</p> </div>
             <div className=''>
         <form>
@@ -24,9 +24,9 @@ function Contact() {
             <input type="password" placeholder="yourname@email.com" className='pt-2 py-2 pl-5 w-10/12 rounded-md bg-[#D0D5DD] outline-slate-500' />
         </div>
 
-        <div className="space-y-2">
+        <div className="space-y-2 mt-5">
             <label className="font-medium text-lg leading-5 mt-5 text-[#344054]">Message</label>
-            <textarea placeholder="Send me a message i will reply as soon as possible" className="pt-2 py-32 pl-5 w-10/12 block rounded-md bg-[#D0D5DD] outline-slate-500"></textarea>
+            <textarea placeholder="Send me a message i will reply as soon as possible" className="pt-2 py-36 pl-5 w-10/12 block rounded-md bg-[#D0D5DD] outline-slate-500"></textarea>
         </div>
 
             </form>
